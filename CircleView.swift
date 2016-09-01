dcbfe373020879068cace8cd2fa47272ccee8cb9
pablodan <pomeally10@gmail.com>
2016-08-31 21:51:39 -0400

    Completed Feed UI

---
+++ CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  Social Network App
+//
+//  Created by Paul Omeally on 8/31/16.
+//  Copyright © 2016 Paul Omeally. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
